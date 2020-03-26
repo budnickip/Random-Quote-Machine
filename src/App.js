715b9611a88d4695fcb402e94b22aa085c211a77
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-03-26 17:10:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            test
+          </div>
+          <div className="col-lg-6">
+            bootstrapa
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
